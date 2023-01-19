39ecafaac9aa10f1fa1dc2947e99018a8c57dd0c
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-19 10:00:21 +0100

    create Comment component comment.jsx

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -68,6 +68,21 @@ const UserPage = ({ userId }) => {
                                 </div>
                             </div>
                         </div>
+                        <div className="col-md-8">
+                            <div className="card mb-2">
+                                {" "}
+                                <div className="card-body ">
+                                    //add comment
+                                </div>
+                            </div>
+                            <div className="card mb-3">
+                                <div className="card-body ">
+                                    <h2>Comments</h2>
+                                    <hr/>
+                                    //comments
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </>
---
+++ src/app/components/ui/qualities/comment.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Comment = (name, time, content) => {
+    return <>
+        <div className="bg-light card-body  mb-3">
+            <div className="row">
+                <div className="col">
+                    <div className="d-flex flex-start ">
+                        <img
+                            src="https://avatars.dicebear.com/api/avataaars/qweqwdas"
+                            className="rounded-circle shadow-1-strong me-3"
+                            alt="avatar"
+                            width="65"
+                            height="65"
+                        />
+                        <div className="flex-grow-1 flex-shrink-1">
+                            <div className="mb-4">
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <p className="mb-1 ">
+                                        {name}
+                                        <span className="small">
+                                            {time}
+                                        </span>
+                                    </p>
+                                    <button className="btn btn-sm text-primary d-flex align-items-center">
+                                        <i className="bi bi-x-lg"></i>
+                                    </button>
+                                </div>
+                                <p className="small mb-0">{content}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div></>;
+};
+Comment.propTypes = {
+    name: PropTypes.string,
+    time: PropTypes.number,
+    content: PropTypes.string
+};
+export default Comment;
