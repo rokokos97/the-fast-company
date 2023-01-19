584c0186407950abe74ce56c3a27c573e4fc38d5
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-01-19 01:18:30 +0100

    restyle users page add button edit user page

--- src/app/components/page/userPage/userPage.jsx
+++ src/app/components/page/userPage/userPage.jsx
@@ -17,15 +17,55 @@ const UserPage = ({ userId }) => {
         return (
             <>
                 <div className="container">
-                    <div className="row">
-                        <div className="card text-center col-md-6 offset-md-3">
-                            <div className="card-body">
-                                <h5 className="card-title">{user.name}</h5>
-                                <p className="card-text">Profession: {user.profession.name}</p>
-                                <p className="card-text"><QualitiesList qualities={user.qualities} /></p>
-                                <p className="card-text">completedMeetings: {user.completedMeetings}</p>
-                                <p className="card-text">Rate: {user.rate}</p>
-                                <button className={"btn btn-primary w-100 wx-auto"} onClick={handleClick}>Edit info</button>
+                    <div className="row gutters-sm">
+                        <div className="col-md-4 mb-3">
+                            <div className="card mb-3">
+                                <div className="card-body">
+                                    <button className="position-absolute top-0 end-0 btn btn-light btn-sm" onClick={handleClick}>
+                                        <i className="bi bi-gear"></i>
+                                    </button>
+                                    <div
+                                        className="d-flex flex-column align-items-center text-center position-relative">
+                                        <img
+                                            src={`https://avatars.dicebear.com/api/avataaars/${(
+                                                Math.random() + 1
+                                            )
+                                                .toString(36)
+                                                .substring(7)}.svg`}
+                                            className="rounded-circle shadow-1-strong me-3"
+                                            alt="avatar"
+                                            width="65"
+                                            height="65"
+                                        />
+                                        <div className="mt-3">
+                                            <h4>{user.name}</h4>
+                                            <p className="text-secondary mb-1">{user.profession.name}</p>
+                                            <div className="text-muted">
+                                                <i className="bi bi-caret-down-fill text-primary" role="button"></i>
+                                                <i className="bi bi-caret-up text-secondary" role="button"></i>
+                                                <span className="ms-2">{user.rate}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="card mb-3">
+                                <div className="card-body d-flex flex-column justify-content-center text-center">
+                                    <h5 className="card-title">
+                                        <span>Qualities</span>
+                                    </h5>
+                                    <p className="card-text">
+                                        <QualitiesList qualities={user.qualities} />
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="card mb-3">
+                                <div className="card-body d-flex flex-column justify-content-center text-center">
+                                    <h5 className="card-title">
+                                        <span>Completed meetings</span>
+                                    </h5>
+                                    <h1 className="display-1">{user.completedMeetings}</h1>
+                                </div>
                             </div>
                         </div>
                     </div>
