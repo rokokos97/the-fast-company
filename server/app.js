c14e56bb0c1e0d32400fc52f5b01db7f6e63c789
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-10 15:34:05 +0100

    create startUp directory; create initDatabase.js; add initDatabase(); import initDatabase() to app.js;

--- server/app.js
+++ server/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const config = require('config');
 const chalk = require('chalk');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose').default;
+const initDatabase = require('./startUp/initDatabase');
 
 const app = express();
 
+app.use(express.json);
+app.use(express.urlencoded({extended: false}));
+
 const PORT = config.get('port') ?? 8080;
-app.listen(PORT, () => {
-  console.log(chalk.green(`Server hes been started on port ${PORT}...`));
-});
+
+async function start() {
+  try {
+    mongoose.connection.once('open', ()=>{
+      initDatabase();
+    });
+    await mongoose.connect(config.get('mongoUri'));
+    app.listen(PORT, () => {
+      console.log(chalk.green(`Server hes been started on port ${PORT}...`));
+    });
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
+
+start();
+
