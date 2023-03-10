1ea1551c1bd4551d658b2c8dc27e834acfbd002c
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-10 16:11:21 +0100

    create models data and import it to initDatabase.js

--- server/startUp/initDatabase.js
+++ server/startUp/initDatabase.js
@@ -1,6 +1,7 @@
 const professionsMock = require('../mock/professions.json');
 const qualitiesMock = require('../mock/qualities.json');
-
+const Professions = require('../models/Profession');
+const Qualities = require('../models/Quality');
 
 module.exports = () => {
 
