519ce5c17911f09b15b786e0d0323bd12abb4d03
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-10 16:43:21 +0100

    add data to db

--- server/startUp/initDatabase.js
+++ server/startUp/initDatabase.js
@@ -3,6 +3,30 @@ const qualitiesMock = require('../mock/qualities.json');
 const Professions = require('../models/Profession');
 const Qualities = require('../models/Quality');
 
-module.exports = () => {
-
+module.exports = async () => {
+  const professions = await Professions.find();
+  if (professions.length !== professionsMock.length) {
+    await createInitialEntity(Professions, professionsMock);
+  }
+  const qualities = await Qualities.find();
+  if (qualities.length !== qualitiesMock.length) {
+    await createInitialEntity(Qualities, qualitiesMock);
+  }
 };
+
+async function createInitialEntity(Model, data) {
+  await Model.collection.drop();
+  return Promise.all(
+      data.map(async (item) => {
+        try {
+          delete item._id;
+          const newItem = new Model(item);
+          await newItem.save();
+          return newItem;
+        } catch (e) {
+          console.log(e.message);
+          return e.message;
+        }
+      }),
+  );
+}
